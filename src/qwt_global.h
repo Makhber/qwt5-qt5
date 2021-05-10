a092229ea78f9fe9761776ad7c7a279c0e46ad23
Mehdi Chinoune <mehdi.chinoune@hotmail.com>
2021-05-10 05:49:17 +0100

    Use Qt Macros for exporting/importing dll symbols.

--- src/qwt_global.h
+++ src/qwt_global.h
@@ -29,10 +29,10 @@
 #ifdef QWT_DLL
 
 #if defined(QWT_MAKEDLL)     // create a Qwt DLL library 
-#define QWT_EXPORT  __declspec(dllexport)
+#define QWT_EXPORT  Q_DECL_EXPORT
 #define QWT_TEMPLATEDLL
 #else                        // use a Qwt DLL library
-#define QWT_EXPORT  __declspec(dllimport)
+#define QWT_EXPORT  Q_DECL_IMPORT
 #endif
 
 #endif // QWT_DLL
